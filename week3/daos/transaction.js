41c21ba792c84c2a08df8905e43cc274ee2075bd
Joel Detweiler <joel.detweiler@gmail.com>
2023-04-25 21:07:16 -0700

    In-class changes to week 3

--- week3/daos/transaction.js
+++ week3/daos/transaction.js
@@ -1,13 +1,24 @@
 const Transaction = require('../models/transaction');
+const mongoose = require('mongoose')
 
 module.exports = {};
 
 module.exports.getAll = (userId, page, perPage) => {
+  console.log({userId})
   return Transaction.find({ userId }).limit(perPage).skip(perPage*page).lean();
 }
 
-module.exports.getById = (userId, transactionId) => {
-  return Transaction.findOne({ _id: transactionId, userId }).lean();
+module.exports.getById = async (userId, transactionId) => {
+  // return Transaction.findOne({ _id: transactionId, userId }).lean();
+  return ( await Transaction.aggregate([
+    { $match: { userId: new mongoose.Types.ObjectId(userId),_id: new mongoose.Types.ObjectId(transactionId)} },
+    { $lookup: {
+      from: 'users',
+      localField: 'userId',
+      foreignField: '_id',
+      as: 'user'
+    }}
+  ]))[0]
 }
 
 module.exports.deleteById = (userId, transactionId) => {
@@ -20,4 +31,12 @@ module.exports.updateById = (userId, transactionId, newObj) => {
 
 module.exports.create = (transactionData) => {
   return Transaction.create(transactionData);
+}
+
+module.exports.getStats = (userId, start, end) => {
+  console.log( { $match: { userId: new mongoose.Types.ObjectId(userId),} })
+  return Transaction.aggregate([
+    { $match: { userId: new mongoose.Types.ObjectId(userId),} },
+    { $group: { _id: '$userId', count: { $sum: 1 }, sum: { $sum: '$charge'} } }
+  ])
 }
