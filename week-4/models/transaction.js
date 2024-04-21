0475a7d178161c1e3dce95d4b95e1d2bc52a47b0
Joel Detweiler <joel.detweiler@gmail.com>
2024-04-20 17:05:44 -0700

    Week 4 updates

--- week-4/models/transaction.js
+++ week-4/models/transaction.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const transactionSchema = new mongoose.Schema({
   date: { type: Date, required: true },
   charge: { type: String, required: true },
-  userId: { type: mongoose.Types.ObjectId, required: true, index: true }
+  userId: { type: String, required: true, index: true },
 });
 
 module.exports = mongoose.model("transactions", transactionSchema);
